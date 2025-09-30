efb4a87ce108db8cc930aeabeeb60d67d4d67e5d
SiHyungLee4076 <karopin4076@gmail.com>
2025-09-30 18:22:05 +0900

    [Bronze IV] Title: 세 수, Time: 176 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int second;
+
+        if ((a >= b && a <= c) || (a <= b && a >= c)) {
+            second = a;
+        } else if ((b >= a && b <= c) || (b <= a && b >= c)) {
+            second = b;
+        } else {
+            second = c;
+        }
+
+        System.out.println(second);
+    }
+}
