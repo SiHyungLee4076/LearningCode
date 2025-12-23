4285d6004980f2eb62d1a20d30dd0c97d9373d1f
SiHyungLee4076 <karopin4076@gmail.com>
2025-12-24 00:24:05 +0900

    [Silver V] Title: 색종이, Time: 184 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] arr = new int[100][100];
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            for (int a = x; a < x + 10; a++) {
+                for (int b = y; b < y + 10; b++) {
+                    arr[a][b] = 1;
+                }
+            }
+        }
+
+        int area = 0;
+
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (arr[i][j] == 1) {
+                    area++;
+                }
+            }
+        }
+
+        System.out.println(area);
+    }
+}
