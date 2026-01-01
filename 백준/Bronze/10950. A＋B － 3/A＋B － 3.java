f821beef2ccc5e9885c7e95438cb6f0c1a0e9794
SiHyungLee4076 <karopin4076@gmail.com>
2026-01-01 11:33:51 +0900

    [Bronze V] Title: A+B - 3, Time: 196 ms, Memory: 18012 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for(int i = 0; i < T; i++) {
+            int num1 = sc.nextInt();
+            int num2 = sc.nextInt();
+            int result = num1 + num2;
+            System.out.println(result);
+        }
+    }
+}
