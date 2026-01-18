fc97bd32878f45771dc5698ea52fea42cfcdf01d
SiHyungLee4076 <karopin4076@gmail.com>
2026-01-19 04:19:14 +0900

    [Bronze II] Title: 대표값2, Time: 172 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[5];
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+
+        Arrays.sort(arr);
+
+        int avg = sum / 5;
+        int med = arr[2];
+
+        System.out.println(avg);
+        System.out.println(med);
+    }
+}
