73e9a88caca780257e4f9e484a3f39da1c1f4aa6
SiHyungLee4076 <karopin4076@gmail.com>
2026-03-09 13:16:05 +0900

    [Silver V] Title: 소트인사이드, Time: 184 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String S = String.valueOf(N);
+
+        char[] arr = S.toCharArray();
+        Arrays.sort(arr);
+
+        for(int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+        }
+    }
+}
